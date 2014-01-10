faf5cc874c315f3ed194f969470e3bde19838523
Liz <lizbrooks@alum.mit.edu>
2014-01-10 12:43:08 -0500

    initial version

--- sensor_data_exploration/rawdata/wunderground.py
+++ sensor_data_exploration/rawdata/wunderground.py
@@ -1,6 +1,40 @@
 #! /usr/bin/env python
 
 # 10.Jan.14 Liz Brooks
-# Collect Wunderground data from Thompson Island station and place in the DB
+# Collect Weather Underground data from Thompson Island station and place in the DB
 
-# testing GIT.....
+import urllib2
+import json
+
+APIkey='2cbf77167bf2fe35'
+stationID='KMABOSTO32'
+
+def getCurrent():
+    url='http://api.wunderground.com/api/'+APIkey+'/conditions/q/pws:'+stationID+'.json'
+    f = urllib2.urlopen(url)
+    json_string = f.read()
+    current = json.loads(json_string)
+    f.close()
+    return current
+
+def getHistory():
+    pass
+
+def writeScreen(current):
+    location = current['current_observation']['observation_location']['city']
+    time=current['current_observation']['observation_time_rfc822']
+    temp_f = current['current_observation']['temp_f']
+    print "Current temperature at %s in %s is: %s F" % (time, location, temp_f)
+
+def writeCSV():
+    pass
+
+def writeDB():
+    pass
+
+def initDB():
+    pass
+
+if __name__ == "__main__":
+    current_conditions = getCurrent()
+    writeScreen(current_conditions)
