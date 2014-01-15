4cc85a547d7695102753c0b48851421763f93889
Linda Kukolich <lindakukolich@alum.mit.edu>
2014-01-15 00:09:35 -0500

    displaying both the temperature data and the canned fake data

--- sensor_data_exploration/sensor_data_exploration/static/sensordata-highcharts.js
+++ sensor_data_exploration/sensor_data_exploration/static/sensordata-highcharts.js
@@ -1,42 +1,36 @@
-//def xdata_chart() {
 $(function () {
-    
+    var chart = sensordata_chart('Air Temperature', 'wu_ti_temp_f', 'degrees Farenheit');
+});
+
+function sensordata_chart(title, subtitle, units) {
+
     var dataArray1 = [];
-    var dataArray2 = [];
-    var dataArray3 = [];
     
-    //    var n_points = context['xdata'].length;
-    //    if (context['ydata'].length < n_points) {
-    //	n_points = context['ydata'].length;
-    //    }
     var n_points = 0;
-    var json_ydata = jsonydata
+    var json_ydata = jQuery.parseJSON(jsonydata);
     console.log("jsonydata =");
     console.log(json_ydata);
     n_points = json_ydata.length;
 
-    console.log("n_points is ")
-    console.log(n_points)
+    console.log("n_points is ");
+    console.log(n_points);
     for (i = 0; i < n_points; i++) {
-	//	dataArray.append([ context['xdata'][i], context['ydata'][i] ]);
 	dataArray1.push( [Date.UTC(1970, 1, i), json_ydata[i]]);
-	dataArray2.push( [Date.UTC(1970, 1, i), i+5]);
-	dataArray3.push( [Date.UTC(1970, 1, i), i-5]);
-	console.log("i is ");
-	console.log(i);
     }
     console.log("dataArray1=");
     console.log(dataArray1);
 	
-    $('#container').highcharts({
+    //    $('#ourdata').highcharts({
+    var chart = new Highcharts.Chart({
             chart: {
+		renderTo: 'ourdata',
                 type: 'spline'
             },
             title: {
-                text: 'Snow depth at Vikjafjellet, Norway'
+                text: title
             },
             subtitle: {
-                text: 'Irregular time data in Highcharts JS'
+                text: subtitle
             },
             xAxis: {
                 type: 'datetime',
@@ -47,30 +41,24 @@ $(function () {
             },
             yAxis: {
                 title: {
-                    text: 'Snow depth (m)'
+                    text: units
                 },
                 min: 0
             },
             tooltip: {
                 formatter: function() {
-                        return '<b>'+ this.series.name +'</b><br/>'+
-                        Highcharts.dateFormat('%e. %b', this.x) +': '+ this.y +' m';
+		    return '<b>'+ this.series.name +'</b><br/>'+
+		    Highcharts.dateFormat('%e. %b', this.x) +': '+ this.y +' m';
                 }
             },
             
             series: [{
-                name: 'Maybe Our data?',
-                // Define the data points. All series have a dummy year
-                // of 1970/71 in order to be compared on the same x axis. Note
-                // that in JavaScript, months start at 0 for January, 1 for February etc.
-                data: dataArray1
-            }, {
-                name: 'Winter 2008-2009',
-                data: dataArray2
-            }, {
-                name: 'Winter 2009-2010',
-                data: dataArray3
-            }]
+		name: 'Maybe Our data?',
+		// Define the data points. All series have a dummy year
+		// of 1970/71 in order to be compared on the same x axis. Note
+		// that in JavaScript, months start at 0 for January, 1 for February etc.
+		data: dataArray1
+	    }]
         });
-    });
-    
+    return chart;
+}
