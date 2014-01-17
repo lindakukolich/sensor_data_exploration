545b5775fa7faad4bdc05ba22ef66a3c803cdecc
Caroline Meeks <caroline@meekshome.com>
2014-01-17 16:31:10 -0500

    Add Charts one under the other

--- sensor_data_exploration/sensor_data_exploration/static/onclick.js
+++ sensor_data_exploration/sensor_data_exploration/static/onclick.js
@@ -5,6 +5,9 @@ $(function () {
     $('.ajax-graph').click(function(){
 	var sensorid;
 	sensorid = $(this).attr('data-sensorid');
+	d=document.createElement('div');
+	d.setAttribute("id", sensorid);
+	$('#charts').append(d)
 	console.log('about to try get_data_ajax and sensorid is:');
 	console.log(sensorid)
 	$.getJSON('/explorer/get_data_ajax/',{'sensorid': sensorid})
@@ -12,7 +15,7 @@ $(function () {
 		console.log('the get_data_ajax ran and the data is:');
 		console.log(data);
 		console.log(data.ydata);
-		var chart = sensordata_chart(data.plot_title, data.plot_subtitle, data.plot_yaxis_label, data.plot_point_label, data.xdata, data.ydata, 'newchart');
+		var chart = sensordata_chart(data.plot_title, data.plot_subtitle, data.plot_yaxis_label, data.plot_point_label, data.xdata, data.ydata, sensorid);
 		
 	    })
 	    .fail(function(jqxhr, textStatus, error) {
