1c2675d0bb357954094ad43d3e33d528291d5755
Caroline Meeks <caroline@meekshome.com>
2014-01-18 09:53:10 -0500

    Removes the graph when you click the button again

--- sensor_data_exploration/sensor_data_exploration/static/onclick.js
+++ sensor_data_exploration/sensor_data_exploration/static/onclick.js
@@ -5,24 +5,27 @@ $(function () {
     $('.btn-default').click(function(){
 	var sensorid;
 	sensorid = $(this).attr('data-sensorid');
-	$(this).addClass('btn-primary');
-	$(this).removeClass('btn-default');
 
-
-	d=document.createElement('div');
-	d.setAttribute("id", sensorid);
-	$('#charts').append(d)
-	console.log('about to try get_data_ajax and sensorid is:');
-	console.log(sensorid)
-	$.getJSON('/explorer/get_data_ajax/',{'sensorid': sensorid})
-	    .done(function(data) {
-		var chart = sensordata_chart(data.plot_title, data.plot_subtitle, data.plot_yaxis_label, data.plot_point_label, data.xdata, data.ydata, sensorid);
+	if ($("#" + sensorid).length != 0) {
+	    $("#" + sensorid).remove()
+	    $(this).addClass('btn-default');
+	    $(this).removeClass('btn-primary');
+	} else {
+	    $(this).addClass('btn-primary');
+	    $(this).removeClass('btn-default');
+	    d=document.createElement('div');
+	    d.setAttribute("id", sensorid);
+	    $('#charts').append(d).show('slow');
+	    $.getJSON('/explorer/get_data_ajax/',{'sensorid': sensorid})
+		.done(function(data) {
+		    var chart = sensordata_chart(data.plot_title, data.plot_subtitle, data.plot_yaxis_label, data.plot_point_label, data.xdata, data.ydata, sensorid);
 		
-	    })
-	    .fail(function(jqxhr, textStatus, error) {
-		var err = textStatus + ", " + error;
-		console.log( "Request Failed: " + err );
-	    });
+		})
+		.fail(function(jqxhr, textStatus, error) {
+		    var err = textStatus + ", " + error;
+		    console.log( "Request Failed: " + err );
+		});
+	};
 	});
 
 });
