3f2294854fc7787c335b8ceceacaa757bbe4eb2a
Caroline Meeks <caroline@meekshome.com>
2014-01-19 15:53:29 -0500

    Set up initial graphs
    I have moved the ajax functionality to its own function and moved it to
    sensordata-highcharts so this is a big commit.

--- sensor_data_exploration/sensor_data_exploration/static/sensordata-highcharts.js
+++ sensor_data_exploration/sensor_data_exploration/static/sensordata-highcharts.js
@@ -32,7 +32,7 @@ function sensordata_chart(title, subtitle, units, short_units, xdata, ydata, ren
             },
             yAxis: {
                 title: {
-                    text: units
+                    text: null
                 }
             },
             tooltip: {
@@ -48,3 +48,30 @@ function sensordata_chart(title, subtitle, units, short_units, xdata, ydata, ren
         });
     return chart;
 }
+
+function ajax_make_chart(sensorid) {
+    $.getJSON('/explorer/get_data_ajax/',{'sensorid': sensorid})
+	.done(function(data) {
+	    //make the div use chart-row template
+	    var chart_source = $('#chart-row').html();
+	    var chart_template = Handlebars.compile(chart_source); //I wonder if I really need to be doing this compile over and over again like this?
+	    var legend_data = {
+		sensorid: sensorid,
+		title: data.plot_title,
+		subtitle: data.plot_subtitle,
+		units: data.plot_yaxis_label
+	    };
+	    $('#charts').append(chart_template(legend_data));
+	    var chart_id = sensorid + "-chart";
+	    if (data.goodPlotData) {
+		var chart = sensordata_chart(data.plot_title, data.plot_subtitle, data.plot_yaxis_label, data.plot_point_label, data.xdata, data.ydata, chart_id);
+	    } else {
+		$('#'+chart_id).append("<br /><b>" + data.plotError +"</b><br />");
+	    }
+	})
+	.fail(function(jqxhr, textStatus, error) {
+	    var err = textStatus + ", " + error;
+	    console.log( "Request Failed: " + err );
+	});
+    $('body').animate({"scrollTop": $(document).height()}, "slow");
+};
