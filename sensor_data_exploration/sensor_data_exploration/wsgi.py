cfe255561387bf6d6b1507673e88684a3c8faede
Caroline Meeks <caroline@meekshome.com>
2014-01-20 18:48:28 -0500

    Heroku specific
    May need to comment some of this out to make this work on local systems.

--- sensor_data_exploration/sensor_data_exploration/wsgi.py
+++ sensor_data_exploration/sensor_data_exploration/wsgi.py
@@ -11,4 +11,9 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sensor_data_exploration.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+#application = get_wsgi_application()
+
+#This copied from Heroku
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
