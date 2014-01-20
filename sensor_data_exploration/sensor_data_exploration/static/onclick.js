71fbbb2bc9a8438cb2f418c1f88a8cce285aecf5
Caroline Meeks <caroline@meekshome.com>
2014-01-20 12:30:02 -0500

    changed the graph btns to look for their own special class
    graph-btn
    fixes a bug where the first created graphs couldn't be removed.

--- sensor_data_exploration/sensor_data_exploration/static/onclick.js
+++ sensor_data_exploration/sensor_data_exploration/static/onclick.js
@@ -11,13 +11,14 @@ $(function () {
 
 	/* If the click came from one of the "sensor" buttons, make a graph
        for that sensor */
-    $('.btn-default').click(function(){
+    $('.graph-btn').click(function(){
 	var sensorid;
 	sensorid = $(this).attr('data-sensorid');
-
+	console.log('button clicked: ' + sensorid)
 	if ($("#" + sensorid).length != 0) { // This checks to see if a div called sensorid exists already.
 	    // To remove a chart. Remove the div. Change the button classes back. 
 	    // I bet this needs to get refactored so that it can also be called when you click the "x" from issue 8.
+	    console.log('about to remove: ' + sensorid);
 	    $("#" + sensorid).remove()
 	    $(this).addClass('btn-default');
 	    $(this).removeClass('btn-primary');
