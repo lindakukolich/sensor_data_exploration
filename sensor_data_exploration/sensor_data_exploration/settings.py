4242bce902887386e158ac9a3f5a16633c53c4a4
Caroline Meeks <caroline@meekshome.com>
2014-01-21 23:59:04 -0500

    pops syntax error in comment

--- sensor_data_exploration/sensor_data_exploration/settings.py
+++ sensor_data_exploration/sensor_data_exploration/settings.py
@@ -98,7 +98,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_DIR,'staticfiles/')  //advice from stackoverflow
+#advice from stack overflow
+STATIC_ROOT = os.path.join(PROJECT_DIR,'staticfiles/') 
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
