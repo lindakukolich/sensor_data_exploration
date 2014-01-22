b8d00cc3e46cf50abf362a44683704c700fd329f
Caroline Meeks <caroline@meekshome.com>
2014-01-21 23:56:52 -0500

    trying to fix static

--- sensor_data_exploration/sensor_data_exploration/settings.py
+++ sensor_data_exploration/sensor_data_exploration/settings.py
@@ -98,7 +98,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(PROJECT_DIR,'staticfiles/')  //advice from stackoverflow
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
